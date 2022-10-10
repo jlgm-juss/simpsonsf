c3319113c82a6ae92ebbe4af63f64a110c310e1c
JLGM <juss.jlgm@gmail.com>
2022-10-09 21:13:06 -0300

    agregamos un spinner como componente condicional ternario

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Button, Container} from "react-bootstrap";
 import Frase from './components/Frase';
 import './style.css';
 import {useEffect, useState} from 'react';
+import Spinner from "./components/Spinner";
+
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
 useEffect(()=>{
   consultarAPI();
@@ -14,17 +17,26 @@ useEffect(()=>{
 const consultarAPI = async() =>{
   
   try {
+    //mostrar el spinner
+    setMostrarSpinner(true)
       const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
       console.log(respuesta);
       const dato = await respuesta.json();
       console.log(dato[0]);
       setPersonaje(dato[0]);
-      
+      //mostrar el componente frase
+      setMostrarSpinner(false);
+
     } catch (error) {
        console.log(error);
       //  mostrar un mensaje al usuario
     }
 }
+
+//condición lógica if, operador ternario ? :
+// (condicion logica)? (logica a ejecutar si cumple la condicion logica): (el codigo que ejecuto si no se cumple la condicion logica)
+
+const mostrarComponente = (mostrarSpinner)?(<Spinner/>):(<Frase personaje={personaje}></Frase>);
 
   return (
     <Container>
@@ -38,7 +50,8 @@ const consultarAPI = async() =>{
       <div className="text-center">
       <Button className="botonNaranja mb-5" variant='warning' onClick={consultarAPI}>Obtener frase</Button>
       </div>
-      <Frase personaje={personaje}></Frase>
+      {mostrarComponente}
+      
     </Container>
   );
 }
